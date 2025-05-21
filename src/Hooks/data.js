4b29018a3a3aa3859bdc315c65711b7684b711d7
Jithin K Satheesh <jithinksatheesh@gmail.com>
2025-05-21 14:07:21 +0530

    2025 update

---
+++ src/Hooks/data.js
@@ -0,0 +1,202 @@
+
+export const data = [
+    {
+        "id": 1,
+        "attributes": {
+            "model": "Cadillac",
+            "brand": "Chevrolet",
+            "seat": "3",
+            "luggage": "3",
+            "createdAt": "2022-08-06T12:27:26.091Z",
+            "updatedAt": "2022-08-09T13:42:58.987Z",
+            "is_available": true,
+            "image": {
+                "data": {
+                    "id": 1,
+                    "attributes": {
+                        "name": "2.png",
+                        "alternativeText": "2.png",
+                        "caption": "2.png",
+                        "width": 631,
+                        "height": 321,
+                        "formats": {
+                            "thumbnail": {
+                                "name": "thumbnail_2.png",
+                                "hash": "thumbnail_2_c206fc1699",
+                                "ext": ".png",
+                                "mime": "image/png",
+                                "path": null,
+                                "width": 245,
+                                "height": 125,
+                                "size": 28.51,
+                                "url": "/uploads/thumbnail_2_c206fc1699.png"
+                            },
+                            "small": {
+                                "name": "small_2.png",
+                                "hash": "small_2_c206fc1699",
+                                "ext": ".png",
+                                "mime": "image/png",
+                                "path": null,
+                                "width": 500,
+                                "height": 254,
+                                "size": 87.06,
+                                "url": "/uploads/small_2_c206fc1699.png"
+                            }
+                        },
+                        "hash": "2_c206fc1699",
+                        "ext": ".png",
+                        "mime": "image/png",
+                        "size": 27.02,
+                        "url": "/uploads/2_c206fc1699.png",
+                        "previewUrl": null,
+                        "provider": "local",
+                        "provider_metadata": null,
+                        "createdAt": "2022-08-06T12:27:21.871Z",
+                        "updatedAt": "2022-08-06T12:27:21.871Z"
+                    }
+                }
+            }
+        }
+    },
+    {
+        "id": 2,
+        "attributes": {
+            "model": "S class",
+            "brand": "Mercedes",
+            "seat": "3",
+            "luggage": "2",
+            "createdAt": "2022-08-09T13:42:38.727Z",
+            "updatedAt": "2022-08-09T13:46:29.109Z",
+            "is_available": true,
+            "image": {
+                "data": {
+                    "id": 2,
+                    "attributes": {
+                        "name": "1.png",
+                        "alternativeText": "1.png",
+                        "caption": "1.png",
+                        "width": 631,
+                        "height": 321,
+                        "formats": {
+                            "thumbnail": {
+                                "name": "thumbnail_1.png",
+                                "hash": "thumbnail_1_2fe08a8838",
+                                "ext": ".png",
+                                "mime": "image/png",
+                                "path": null,
+                                "width": 245,
+                                "height": 125,
+                                "size": 25.47,
+                                "url": "/uploads/thumbnail_1_2fe08a8838.png"
+                            }
+                        },
+                        "hash": "1_2fe08a8838",
+                        "ext": ".png",
+                        "mime": "image/png",
+                        "size": 97.57,
+                        "url": "/uploads/1_2fe08a8838.png",
+                        "previewUrl": null,
+                        "provider": "local",
+                        "provider_metadata": null,
+                        "createdAt": "2022-08-09T13:41:27.305Z",
+                        "updatedAt": "2022-08-09T13:41:27.305Z"
+                    }
+                }
+            }
+        }
+    },
+    {
+        "id": 3,
+        "attributes": {
+            "model": "5 series",
+            "brand": "BMW",
+            "seat": "3",
+            "luggage": "2",
+            "createdAt": "2022-08-09T13:46:21.423Z",
+            "updatedAt": "2022-08-09T13:46:21.423Z",
+            "is_available": true,
+            "image": {
+                "data": {
+                    "id": 4,
+                    "attributes": {
+                        "name": "3.png",
+                        "alternativeText": "3.png",
+                        "caption": "3.png",
+                        "width": 631,
+                        "height": 321,
+                        "formats": {
+                            "thumbnail": {
+                                "name": "thumbnail_3.png",
+                                "hash": "thumbnail_3_043815e680",
+                                "ext": ".png",
+                                "mime": "image/png",
+                                "path": null,
+                                "width": 245,
+                                "height": 125,
+                                "size": 30.98,
+                                "url": "/uploads/thumbnail_3_043815e680.png"
+                            }
+                        },
+                        "hash": "3_043815e680",
+                        "ext": ".png",
+                        "mime": "image/png",
+                        "size": 99.03,
+                        "url": "/uploads/3_043815e680.png",
+                        "previewUrl": null,
+                        "provider": "local",
+                        "provider_metadata": null,
+                        "createdAt": "2022-08-09T13:41:51.584Z",
+                        "updatedAt": "2022-08-09T13:41:51.584Z"
+                    }
+                }
+            }
+        }
+    },
+    {
+        "id": 4,
+        "attributes": {
+            "model": "800 e63",
+            "brand": "Brabus",
+            "seat": "2",
+            "luggage": "1",
+            "createdAt": "2022-08-09T13:49:40.994Z",
+            "updatedAt": "2022-08-09T13:49:40.994Z",
+            "is_available": true,
+            "image": {
+                "data": {
+                    "id": 5,
+                    "attributes": {
+                        "name": "4.png",
+                        "alternativeText": "4.png",
+                        "caption": "4.png",
+                        "width": 631,
+                        "height": 321,
+                        "formats": {
+                            "thumbnail": {
+                                "name": "thumbnail_4.png",
+                                "hash": "thumbnail_4_e4c83de7b9",
+                                "ext": ".png",
+                                "mime": "image/png",
+                                "path": null,
+                                "width": 245,
+                                "height": 125,
+                                "size": 28.66,
+                                "url": "/uploads/thumbnail_4_e4c83de7b9.png"
+                            }
+                        },
+                        "hash": "4_e4c83de7b9",
+                        "ext": ".png",
+                        "mime": "image/png",
+                        "size": 94.13,
+                        "url": "/uploads/4_e4c83de7b9.png",
+                        "previewUrl": null,
+                        "provider": "local",
+                        "provider_metadata": null,
+                        "createdAt": "2022-08-09T13:49:38.215Z",
+                        "updatedAt": "2022-08-09T13:49:38.215Z"
+                    }
+                }
+            }
+        }
+    }
+]
--- src/Hooks/initCars.js
+++ src/Hooks/initCars.js
@@ -2,7 +2,7 @@ import React, { useState, useContext } from 'react'
 import axios from 'axios'
 
 import { Store } from 'Store/orderStore'
-
+import {data} from './data'
 export default function InitCars(props) {
 
     const _store = useContext(Store)
@@ -11,9 +11,10 @@ export default function InitCars(props) {
     const initCars = async() => {
         try {
 
-            const res = await axios.get(`${process.env.REACT_APP_BASE_URL}/api/cars?populate=*`)
+            // const res = await axios.get(`${process.env.REACT_APP_BASE_URL}/api/cars?populate=*`)
             // console.log(res?.data?.data)
-            const list = res?.data?.data || []
+            // const list = res?.data?.data || []
+            const list = data
             const carlist = list.filter(item => item.attributes.is_available === true).map(item => ({
                 id:item.id, 
                 ...item.attributes,
